8c8609f506e6d48bc5cb72fba6ff6d594d2d66e5
Cayla07 <maicaeupinado@gmail.com>
2025-04-26 22:00:18 +0700

    Partially Done Recycle Functionality

--- AdminDB.Designer.cs
+++ AdminDB.Designer.cs
@@ -28,219 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDB));
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
-            panel4 = new Panel();
-            panel3 = new Panel();
-            pictureBox1 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            button4 = new Button();
-            button3 = new Button();
-            button2 = new Button();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            FLPanelUser = new FlowLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // label4
+            // tableLayoutPanel1
             // 
-            label4.Anchor = AnchorStyles.None;
-            label4.AutoSize = true;
-            label4.Font = new Font("Arial Black", 13.8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label4.ForeColor = Color.DarkGreen;
-            label4.Location = new Point(106, 47);
-            label4.Name = "label4";
-            label4.Size = new Size(194, 32);
-            label4.TabIndex = 44;
-            label4.Text = "Waste Tracker";
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(FLPanelUser, 0, 3);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.357231F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 44.64402F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5.35723066F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 44.6415176F));
+            tableLayoutPanel1.Size = new Size(1129, 679);
+            tableLayoutPanel1.TabIndex = 0;
             // 
-            // label3
+            // FLPanelUser
             // 
-            label3.Anchor = AnchorStyles.None;
-            label3.AutoSize = true;
-            label3.Font = new Font("Arial Black", 10.8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.DarkGreen;
-            label3.Location = new Point(534, 345);
-            label3.Name = "label3";
-            label3.Size = new Size(107, 26);
-            label3.TabIndex = 43;
-            label3.Text = "Statistics";
-            // 
-            // label2
-            // 
-            label2.Anchor = AnchorStyles.None;
-            label2.AutoSize = true;
-            label2.Font = new Font("Arial Black", 10.8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.DarkGreen;
-            label2.Location = new Point(106, 345);
-            label2.Name = "label2";
-            label2.Size = new Size(101, 26);
-            label2.TabIndex = 42;
-            label2.Text = "User List";
-            // 
-            // panel4
-            // 
-            panel4.Anchor = AnchorStyles.None;
-            panel4.BackColor = Color.White;
-            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
-            panel4.BackgroundImageLayout = ImageLayout.Center;
-            panel4.Location = new Point(530, 377);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(676, 312);
-            panel4.TabIndex = 41;
-            // 
-            // panel3
-            // 
-            panel3.Anchor = AnchorStyles.None;
-            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
-            panel3.BackgroundImageLayout = ImageLayout.Center;
-            panel3.Location = new Point(106, 377);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(418, 312);
-            panel3.TabIndex = 40;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Anchor = AnchorStyles.None;
-            pictureBox1.BackColor = Color.White;
-            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(106, 82);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(251, 227);
-            pictureBox1.TabIndex = 32;
-            pictureBox1.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.Anchor = AnchorStyles.None;
-            pictureBox5.BackColor = Color.Transparent;
-            pictureBox5.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox5.Location = new Point(106, 82);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(251, 227);
-            pictureBox5.TabIndex = 39;
-            pictureBox5.TabStop = false;
-            // 
-            // button4
-            // 
-            button4.Anchor = AnchorStyles.None;
-            button4.BackColor = Color.White;
-            button4.BackgroundImageLayout = ImageLayout.Center;
-            button4.Image = (Image)resources.GetObject("button4.Image");
-            button4.Location = new Point(955, 82);
-            button4.Name = "button4";
-            button4.Size = new Size(251, 227);
-            button4.TabIndex = 38;
-            button4.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.Anchor = AnchorStyles.None;
-            button3.BackColor = Color.White;
-            button3.BackgroundImageLayout = ImageLayout.Center;
-            button3.Image = (Image)resources.GetObject("button3.Image");
-            button3.Location = new Point(674, 82);
-            button3.Name = "button3";
-            button3.Size = new Size(251, 227);
-            button3.TabIndex = 37;
-            button3.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.Anchor = AnchorStyles.None;
-            button2.BackColor = Color.White;
-            button2.BackgroundImageLayout = ImageLayout.Center;
-            button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.Location = new Point(390, 82);
-            button2.Name = "button2";
-            button2.Size = new Size(251, 227);
-            button2.TabIndex = 36;
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Anchor = AnchorStyles.None;
-            pictureBox4.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox4.Location = new Point(955, 82);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(251, 227);
-            pictureBox4.TabIndex = 35;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Anchor = AnchorStyles.None;
-            pictureBox3.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox3.Location = new Point(674, 82);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(251, 227);
-            pictureBox3.TabIndex = 34;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Anchor = AnchorStyles.None;
-            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox2.Location = new Point(390, 82);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(251, 227);
-            pictureBox2.TabIndex = 33;
-            pictureBox2.TabStop = false;
+            FLPanelUser.AutoScroll = true;
+            FLPanelUser.Location = new Point(3, 378);
+            FLPanelUser.Name = "FLPanelUser";
+            FLPanelUser.Size = new Size(558, 298);
+            FLPanelUser.TabIndex = 0;
             // 
             // AdminDB
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.PaleGreen;
-            ClientSize = new Size(1313, 736);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(panel4);
-            Controls.Add(panel3);
-            Controls.Add(pictureBox1);
-            Controls.Add(pictureBox5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
+            ClientSize = new Size(1153, 703);
+            Controls.Add(tableLayoutPanel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "AdminDB";
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
             Load += AdminDB_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Label label4;
-        private Label label3;
-        private Label label2;
-        private Panel panel4;
-        private Panel panel3;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private FlowLayoutPanel FLPanelUser;
     }
 }
